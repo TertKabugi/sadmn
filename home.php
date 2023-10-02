e2ca4643433960117c44510a2a9a0d89f5bc42e5
Tertius Kabugi <kingkabugi@gmail.com>
2023-10-02 18:04:22 +0300

    sadmn

---
+++ home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Simple Web Page</title>
+    <style>
+        /* Basic CSS styling */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 20px;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: white;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+    </style>
+</head>
+<body>
+<header>
+    <h1>Server Administration</h1>
+</header>
+<div class="container">
+    <h2>Group Work</h2>
+    <p>
+        This is a simple web page created using HTML and CSS for Server Administration Group 15.
+    </p>
+   </div>
+</body>
+</html>
